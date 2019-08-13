382bf69b4731794a5f9d613d7aa2d628e1b774b0
ginnac <ginnacampbell1990@gmail.com>
2019-08-12 18:35:59 -0600

    changes done - logic working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,7 +19,41 @@ app.get("/api/friends", function(req,res){
 app.post("/api/friends",function(req,res){
 
     friends.push(req.body);  
-  var le =  friends.length - 1 
+
+   //logic to find friend
+ 
+   var differenceArray = [];
+   var friendsList =  friends.length - 1;
+   var totalDifference = 0;
+   
+   for (var i=0;i<friendsList.length;i++){
+    
+     var score = friends[i].scores;
+     var yourScore = friends[friendsList].scores;
+     function sum(){
+     totalDifference = 0; 
+     for (var x=0; x < score.length; x++){
+
+   
+        if (!score[x]===yourScore[x]){
+            var difference = 0;
+            difference = score[x] - yourScore[x]
+            difference = Math.abs(difference);
+            totalDifference += difference
+
+
+        }  
+
+        else{ totalDifference += 0}
+    
+     };
+
+     return totalDifference;
+    };
+
+    differenceArray.push(totalDifference);
+
+   };
    res.json(friends[le]);
     
 });
